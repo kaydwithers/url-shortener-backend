bb7156d819a3335a4567ce9c00b34a3261d810b0
kaydwithers <kayd.withers@isobar.com>
2022-06-26 20:01:52 +1000

    feat: undo typescript file.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 const express = require("express");
 const mongoose = require("mongoose");
-const ShortUrl = require("../models/short-url.ts");
+const ShortUrl = require("../models/short-url");
 
 const app = express();
 
